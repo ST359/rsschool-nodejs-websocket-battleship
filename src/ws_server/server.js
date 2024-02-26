72727223a7886241b2ea431af29e56b704a2fd76
ST359 <135271126+ST359@users.noreply.github.com>
2024-02-27 00:44:13 +0300

    feat: implement add user to room

--- src/ws_server/server.js
+++ src/ws_server/server.js
@@ -4,9 +4,10 @@ export const wsServer = new WebSocketServer({ port: 3000 });
 
 /*user-> pass, login, connection, id??, room??, wins?? */
 
-const users = [];
-const availableRooms = [];
-const fullRooms = [];
+let users = [];
+let availableRooms = [];
+let fullRooms = [];
+let currentGames = [];
 
 wsServer.on("connection", (connection) => {
     connection.connectionId = randomUUID();
@@ -19,8 +20,8 @@ wsServer.on("connection", (connection) => {
 });
 
 const handleMessage = (msgType, parsedData, connection) => {
-    let senderUser = {}
-    senderUser = users.find((user)=>user.userId === connection.connectionId);
+    let senderUser = {};
+    senderUser = users.find((user) => user.userId === connection.connectionId);
     let response = {};
     switch (msgType) {
         case "reg":
@@ -45,48 +46,129 @@ const handleMessage = (msgType, parsedData, connection) => {
             connection.send(JSON.stringify(response));
             break;
         case "create_room":
-            if(isUserInRoom(senderUser) === false){
+            if (isUserInRoom(senderUser) === false) {
                 createRoom(senderUser);
             }
             updateRoom();
+            break;
+        case "add_user_to_room":
+            addUserToRoom(senderUser, parsedData.indexRoom);
+            updateRoom();
+            break;
+        case "add_ships":
+            addShips(senderUser, parsedData.ships);
+            let room = findRoom(senderUser);
+            console.log(senderUser);
+            console.log(room);
+            if (areBoardsSet(room)) {
+                startGame(senderUser);
+            }
             break;
         default:
             return 0;
     }
 };
 
-const createRoom = (senderUser) =>{
-    const roomId = randomUUID();
-    const room = {roomId: roomId, roomUsers:[{name: senderUser.name, index: senderUser.userId}]};
-    availableRooms.push(room);
-}
+const startGame = (user) => {
+    user.connection.send(
+        JSON.stringify({
+            type: "start_game",
+            data: JSON.stringify({
+                ships: user.ships,
+                currentPlayerIndex: user.userId,
+            }),
+            id: 0,
+        })
+    );
+};
 
-const updateRoom = ()=>{
+const addShips = (user, ships) => {
+    user.ships = ships;
+    user.boardSet = true;
+};
+const areBoardsSet = (room) => {
+    let count = 0;
+    room.roomUsers.forEach((roomUser) => {
+        if (users.find((user) => user.userId === roomUser.index).boardSet) {
+            count++;
+        }
+    });
+    if (count === 2) {
+        return true;
+    }
+};
+
+const findRoom = (user) => {
+    console.log(fullRooms);
+    fullRooms.forEach((room) => {
+        room.roomUsers.forEach((roomUser) => {
+            if (user.userId === roomUser.index) {
+                return room;
+            }
+        });
+    });
+};
+const addUserToRoom = (user, roomIndex) => {
+    availableRooms.forEach((room) => {
+        if (room.roomId === roomIndex) {
+            room.roomUsers.push({ name: user.name, index: user.userId });
+            fullRooms.push(room);
+            createGame(room);
+            const index = availableRooms.indexOf(room);
+            availableRooms.splice(index, 1);
+            return 1;
+        }
+    });
+};
+const createGame = (room) => {
+    room.roomUsers.forEach((roomUser) =>
+        users
+            .find((user) => user.userId === roomUser.index)
+            .connection.send(
+                JSON.stringify({
+                    type: "create_game",
+                    data: JSON.stringify({
+                        idGame: room.roomId,
+                        idPlayer: roomUser.index,
+                    }),
+                    id: 0,
+                })
+            )
+    );
+};
+const createRoom = (senderUser) => {
+    const roomId = randomUUID();
+    const room = {
+        roomId: roomId,
+        roomUsers: [{ name: senderUser.name, index: senderUser.userId }],
+    };
+    availableRooms.push(room);
+};
+
+const updateRoom = () => {
     const roomUpdateMsg = {
         type: "update_room",
-        //maybe JSON.stringify will be needed
         data: JSON.stringify(availableRooms),
         id: 0,
-    }
-    wsServer.clients.forEach((connection) => {connection.send(JSON.stringify(roomUpdateMsg))})
-}
-const isUserInRoom = (user) =>{
+    };
+    wsServer.clients.forEach((connection) => {
+        connection.send(JSON.stringify(roomUpdateMsg));
+    });
+};
+const isUserInRoom = (user) => {
     let flag = false;
-    availableRooms.forEach((room) =>{
-        room.roomUsers.forEach((roomUser)=>{
-            if(roomUser.index.toString() === user.userId.toString()){
+    availableRooms.forEach((room) => {
+        room.roomUsers.forEach((roomUser) => {
+            if (roomUser.index.toString() === user.userId.toString()) {
                 flag = true;
             }
-        })
+        });
     });
     return flag;
-}
+};
 const createUser = (name, password, connection, connectionId) => {
     users.forEach((user) => {
-        if (
-            user.name === name &&
-            user.password === password
-        ) {
+        if (user.name === name && user.password === password) {
             return user;
         }
     });
@@ -95,6 +177,8 @@ const createUser = (name, password, connection, connectionId) => {
         password: password,
         userId: connectionId,
         connection: connection,
+        boardSet: false,
+        ships: [],
     };
     return newUser;
 };
